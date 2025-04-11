e7ec67645382261bc31fd959ecbe85672d9331ce
iamaray <arya.nahri@gmail.com>
2025-04-11 18:27:07 -0400

    file structure for parser

---
+++ parser/ast.h
@@ -0,0 +1,14 @@
+typedef enum
+{
+    AST_ASSIGNMENT,
+    AST_PRINT,
+    AST_WHILE,
+    AST_BLOCK,
+    AST_BINARY,
+    AST_INT_LITERAL,
+    AST_VARIABLE
+} ASTNodeType;
+
+// typedef struct ASTNode {
+
+// }
